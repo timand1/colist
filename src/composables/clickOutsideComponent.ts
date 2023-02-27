6342f266572e148196a845c8ad617fc180fbf480
timand1 <80768929+timand1@users.noreply.github.com>
2023-02-27 08:49:23 +0100

    Fix clickOutsideComponent func

---
+++ src/composables/clickOutsideComponent.ts
@@ -0,0 +1,17 @@
+import {onBeforeUnmount, onMounted, type Ref} from 'vue'
+
+export default function useDetectOutsideClick(component:Ref, callback:() => void) {
+    if (!component) return
+    const listener = (event: Event) => {
+        if (event.target !== component.value && event.composedPath().includes(component.value)) {
+            return
+        }
+        if (typeof callback === 'function') {
+            callback()
+        }
+    }
+    onMounted(() => { window.addEventListener('click', listener) })
+    onBeforeUnmount(() => {window.removeEventListener('click', listener)})
+
+    return {listener}
+}
