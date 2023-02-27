9050aef5ebc114bd7f3c6af894fe3305f4f8e72f
timand1 <80768929+timand1@users.noreply.github.com>
2023-02-27 18:54:18 +0100

    Update router import to import correct file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/home.vue'
-import Login from '@/views/home/home.vue'
+import Login from '@/views/login/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
