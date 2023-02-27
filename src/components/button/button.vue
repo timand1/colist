f767a28fa34098fa3ac797eecf0d9c7fc07b6fa7
timand1 <80768929+timand1@users.noreply.github.com>
2023-02-27 09:08:28 +0100

    button v1

---
+++ src/components/button/button.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, type Ref } from 'vue';
+import { buttonVariants, defaultVariantMixin } from '@/helpers/mixins/jsMixins';
+
+type ButtonProps = {
+    text?: string
+    variant?: string
+    outline: boolean
+
+};
+const props = withDefaults(defineProps<ButtonProps>(), {
+    text: 'Text',
+    variant: 'primary',
+    outline: false
+});
+const emit = defineEmits(['click']);
+
+const componentRef = ref();
+const buttonVariant = ref<string>(props.variant)
+const buttonClass = ref<string>('');
+
+// Check if variant prop is valid
+defaultVariantMixin(buttonVariants).verifyVariant(props.variant)
+  ? ''
+  : (buttonVariant.value = 'primary');
+
+onMounted(() => {
+    props.outline 
+    ? buttonClass.value = 'button--' + buttonVariant.value + '__outline'
+    : buttonClass.value = 'button--' + buttonVariant.value
+})
+
+</script>
+
+<template>
+  <button
+    ref="componentRef"
+    class="button"
+    :class="buttonClass, props.outline ? 'outline' : ''"
+    @click="emit('click')"
+    @keyup.enter="emit('click')"
+  >
+  {{ props.text }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import "@/components/button/button.scss";
+</style>
