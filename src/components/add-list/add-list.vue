16b4f89394e3484d9ad0fabc2f59e0cce690463e
timand1 <timandersson22@live.se>
2023-02-28 15:20:20 +0100

    Create list and get the list working

--- src/components/add-list/add-list.vue
+++ src/components/add-list/add-list.vue
@@ -1,11 +1,77 @@
 <script setup lang="ts">
-</script>
-<template>
-  <div>
+import { ref } from 'vue';
+import { listTypes } from '@/helpers/mixins/jsMixins';
+import Button from '@/components/button/button.vue';
+import useDetectOutsideClick from '@/composables/clickOutsideComponent';
+import { setActiveListId } from '@/composables/activeList';
+import { doc, setDoc } from "firebase/firestore"; 
+import { auth, db } from '@/firebase';
+import { useRouter } from "vue-router";
 
-  </div>
+
+type AddItemProps = {
+  displayOverlay: boolean
+};
+
+const props = withDefaults(defineProps<AddItemProps>(), {
+
+});
+
+const emit = defineEmits(['click']);
+
+const listname = ref<string>('')
+const listtype = ref<string>('')
+const addItemRef = ref()
+
+const router = useRouter()
+
+const createNewList: () => Promise<void> = async () => {
+  
+  const newList = {
+    author : auth.currentUser?.displayName,
+    name: listname.value,
+    id : crypto.randomUUID(),
+    list: [],
+    users: []
+  }
+  console.log(listname.value?.length );
+  
+  if(listname.value?.length > 2 && listtype.value?.length > 0) {
+    await setDoc(doc(db, "lists", newList.id), {
+      newList
+    })
+    setActiveListId(newList.id)
+    router.push(`/list`)
+  } else {
+    console.log('wtf dude');
+    
+  }
+}
+
+useDetectOutsideClick(addItemRef, () => { 
+  props.displayOverlay ?
+  emit('click')
+  : null
+})
+
+</script>
+
+<template>
+  <section class="add-list__overlay">
+    <section class="add-list" ref="addItemRef">
+      {{ props.displayOverlay }}
+      <input type="text" name="listname" v-model="listname">
+      <select name="listtype" v-model="listtype">
+        <option v-for="list in listTypes" :value="list">{{ list }}</option>
+    </select>
+    <div class="button-container">
+      <Button variant="danger" text="Cancel" @click="emit('click')" />
+      <Button variant="primary" text="Add" @click="createNewList" />
+    </div>
+  </section>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-
+  @import '@/components/add-list/add-list.scss';
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/home.vue'
 import Login from '@/views/login/login.vue'
+import List from '@/views/list/list.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
     }
   ]
 })
