20b334c9bebe4c642e721cd788e1feae7c34b041
timand1 <80768929+timand1@users.noreply.github.com>
2023-03-01 09:59:50 +0100

    add item init files

---
+++ src/components/add-item/add-item.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@/components/add-item/add-item.scss';
+</style>
