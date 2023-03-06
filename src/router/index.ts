9d3889e7251187f9150c518a9de32b6b4b00a4d0
timand1 <80768929+timand1@users.noreply.github.com>
2023-03-06 14:57:38 +0100

    netlify test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '@/views/home/home.vue'
 import Login from '@/views/login/login.vue'
 import List from '@/views/list/list.vue'
 import Errorpage from '@/views/errorpage/errorpage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory (import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
